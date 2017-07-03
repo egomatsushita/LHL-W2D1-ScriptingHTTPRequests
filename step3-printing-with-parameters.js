f279cf0e45880fb66555b2dea60f14f5eedf866a
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-03 16:04:14 +0000

    Add a parameter to getAndPrintHTML function

--- step3-printing-with-parameters.js
+++ step3-printing-with-parameters.js
@@ -1,1 +1,30 @@
-step3-printing-with-parameters.js
+var https = require("https");
+
+function getAndPrintHTML(options) {
+  var info = [];
+
+  var aRequestOptions = options;
+
+  https.get(aRequestOptions, function(response) {
+    // set encoding of received data to utf-8
+    response.setEncoding("utf8");
+    // the callback is invoked when a data chunk is received
+    response.on('data', function(data) {
+      info.push(data);
+    });
+
+    response.on('end', function() {
+      for(var data of info) {
+        console.log(data);
+      }
+      console.log("Response stream complete.");
+    });
+  });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions);
