82fe1f7972ccc2bb89a96613e5d5c44e8e45b26b
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-03 15:52:01 +0000

    Add a variable info that holds all the buffered data

--- step2-printing-with-buffered-strings.js
+++ step2-printing-with-buffered-strings.js
@@ -1,11 +1,27 @@
 var https = require("https");
-var fs = require("fs");
 
 function getAndPrintHTML() {
+  var info = [];
   var requestOptions = {
     host: 'sytatris.github.io',
     path: '/http-examples/step2.html'
   };
 
+  https.get(requestOptions, function(response) {
+    // set encoding of received data to utf-8
+    response.setEncoding("utf8");
+    // the callback is invoked when a data chunk is received
+    response.on('data', function(data) {
+      info.push(data);
+    });
 
+    response.on('end', function() {
+      for(var data of info) {
+        console.log(data);
+      }
+      console.log("Response stream complete.");
+    });
+  });
 }
+
+getAndPrintHTML();
