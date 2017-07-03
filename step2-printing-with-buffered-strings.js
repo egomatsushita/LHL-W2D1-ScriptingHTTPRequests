7f020a661978921bd8c8c5c33975e645121f0e2a
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-03 14:54:53 +0000

    First commit

---
+++ step2-printing-with-buffered-strings.js
@@ -0,0 +1,11 @@
+var https = require("https");
+var fs = require("fs");
+
+function getAndPrintHTML() {
+  var requestOptions = {
+    host: 'sytatris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+
+}
