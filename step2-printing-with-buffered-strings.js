e2ab09fae83523809dc74627aca0429d881ab31e
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-04 02:35:30 +0000

    Correct host property value - typo problem

--- step2-printing-with-buffered-strings.js
+++ step2-printing-with-buffered-strings.js
@@ -1,25 +1,30 @@
-var https = require("https");
-
 function getAndPrintHTML() {
+
+  var https = require("https");
+
   var info = [];
+
   var requestOptions = {
-    host: 'sytatris.github.io',
+    host: 'sytantris.github.io',
     path: '/http-examples/step2.html'
   };
 
   https.get(requestOptions, function(response) {
+
     // set encoding of received data to utf-8
     response.setEncoding("utf8");
+
     // the callback is invoked when a data chunk is received
     response.on('data', function(data) {
+      // info += data;
       info.push(data);
     });
 
     response.on('end', function() {
-      for(var data of info) {
-        console.log(data);
-      }
       console.log("Response stream complete.");
+      for(var content of info) {
+        console.log("Content: ", content);
+      }
     });
   });
 }
