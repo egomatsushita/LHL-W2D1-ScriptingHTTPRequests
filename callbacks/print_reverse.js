d504b173ab26727e59ae194376e75793e882b21c
Eduardo Go <eduardo.go.rj@gmail.com>
2017-07-04 02:44:05 +0000

    Correct typo on the path property

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -2,11 +2,11 @@ var getHTML = require("./http-functions.js");
 
 var requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step6/uppercase.html'
+  path: '/http-examples/step6/reverse.html'
 };
 
 function printReverse(html) {
-  var infoReverse = html.split(" ").reverse().join("");
+  var infoReverse = html.split("").reverse().join("");
   console.log(infoReverse);
 }
 
